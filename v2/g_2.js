81e9c559c9dc9e9c8a2724334e0640d60bdcac8e
Juan <juanpablo.1033487096@gmail.com>
2022-10-25 00:24:50 +0000

    Update g_2.js

--- v2/g_2.js
+++ v2/g_2.js
@@ -1,72 +1,85 @@
 //Clase para crea la partida.
 class Game {
-    gameId = Math.floor(Math.random()*10001);
-    isOnSpree = [false, "", 0];
-    playersDeath = 0;
-    //La cantidad de asesinatos que un jugador hizo durante la partida.
-    totalKills (killer){
-        console.log(`El jugador ${killer.nickName} Mato a ${killer.killCount} Jugadores`);
-    }
-    //Los jugadores asesinados por una persona en especifico.
-    totalKills_Names (killer){
-        console.log(`El jugador ${killer.nickName} Mato a los jugadores: ${killer.killCountNames}`);
-    }
-    killingSpree (){
-        if (this.isOnSpree[0]) {
-            if (this.isOnSpree[2] === 2) console.log(this.isOnSpree[1]);
-            if (this.isOnSpree[2] === 3) console.log(`${this.isOnSpree[1]} con una TripleKill!`);
-            if (this.isOnSpree[2] === 4) console.log(`${this.isOnSpree[1]} con una CuadraKill!`);
-            if (this.isOnSpree[2] === 5) console.log(`${this.isOnSpree[1]} con una PentaKill!`);
-        } else console.warn("No hay racha de ningun jugador...");
-    }
+  gameId = Math.floor(Math.random() * 10001);
+  isOnSpree = [false, "", 0];
+  playersDeath = 0;
+  //La cantidad de asesinatos que un jugador hizo durante la partida.
+  totalKills(killer) {
+    console.log(
+      `El jugador ${killer.nickName} Mato a ${killer.killCount} Jugadores`
+    );
+  }
+  //Los jugadores asesinados por una persona en especifico.
+  totalKills_Names(killer) {
+    console.log(
+      `El jugador ${killer.nickName} Mato a los jugadores: ${killer.killCountNames}`
+    );
+  }
+  killingSpree() {
+    if (this.isOnSpree[0]) {
+      if (this.isOnSpree[2] === 2) console.log(this.isOnSpree[1]);
+      if (this.isOnSpree[2] === 3)
+        console.log(`${this.isOnSpree[1]} con una TripleKill!`);
+      if (this.isOnSpree[2] === 4)
+        console.log(`${this.isOnSpree[1]} con una CuadraKill!`);
+      if (this.isOnSpree[2] === 5)
+        console.log(`${this.isOnSpree[1]} con una PentaKill!`);
+    } else console.warn("No hay racha de ningun jugador...");
+  }
 }
 //Se crea una nueva partida.
-let game_1 = new Game;
+let game_1 = new Game();
 //Clase para crear a los jugadores.
-class Player { 
-    id = Math.floor(Math.random() * 101);
-    killCount = 0;
-    killCountNames = [];
-    //Parametros personalizados para cada jugador.
-    constructor (nickName, isAlive, skin){
-        this.nickName = nickName;
-        this.isAlive = isAlive;
-        this.skin = skin;
-    }
-    spree = 0;
-    lastKiller = "";
-    //Funcion para que el jugador mate a otros jugadores.
-    kill (playerSlain) {
-        //Racha de asesnatos.
-        if (this.lastKiller === this.nickName) {
-            game_1.isOnSpree[0] = true;
-            game_1.isOnSpree[1] = `${this.nickName} esta en racha de asesinatos`;
-            game_1.isOnSpree[2] = this.spree++;
-        } else game_1.isOnSpree[0] = false;
-        //Si el jugador que esta intentando matar ya esta muerto.
-        if (!this.isAlive) console.log("El jugador que intetas usar ya esta muerto, este no puede matar a nadie"); 
+class Player {
+  id = Math.floor(Math.random() * 101);
+  killCount = 0;
+  killCountNames = [];
+  //Parametros personalizados para cada jugador.
+  constructor(nickName, isAlive, skin) {
+    this.nickName = nickName;
+    this.isAlive = isAlive;
+    this.skin = skin;
+  }
+  spree = 0;
+  lastKiller = "";
+  //Funcion para que el jugador mate a otros jugadores.
+  kill(playerSlain) {
+    //Racha de asesnatos.
+    if (this.lastKiller === this.nickName) {
+      game_1.isOnSpree[0] = true;
+      game_1.isOnSpree[1] = `${this.nickName} esta en racha de asesinatos`;
+      game_1.isOnSpree[2] = this.spree++;
+    } else game_1.isOnSpree[0] = false;
+    //Si el jugador que esta intentando matar ya esta muerto.
+    if (!this.isAlive)
+      console.log(
+        "El jugador que intetas usar ya esta muerto, este no puede matar a nadie"
+      );
+    else {
+      const killResume = () => {
+        console.log(`${this.nickName} A asesinado a ${playerSlain.nickName}`);
+        playerSlain.isAlive = false;
+        game_1.playersDeath++;
+        this.killCount++;
+        this.killCountNames.push(playerSlain.nickName);
+      };
+      //Si es el primer asesinato de la partiada
+      if (game_1.playersDeath === 0) {
+        killResume();
+        this.lastKiller = this.nickName;
+      } else {
+        //Revisar si el jugador al que se intenta matar esta aun vivo.
+        if (!playerSlain.isAlive)
+          console.log(
+            `Hey ${playerSlain.nickName} Ya fue asesinado, la operacion no se puede completar`
+          );
         else {
-            const killResume = () => {
-                console.log(`${this.nickName} A asesinado a ${playerSlain.nickName}`);
-                playerSlain.isAlive = false;
-                game_1.playersDeath++;
-                this.killCount++;
-                this.killCountNames.push(playerSlain.nickName);
-            }
-            //Si es el primer asesinato de la partiada
-            if (game_1.playersDeath === 0) {
-                killResume();
-                this.lastKiller = this.nickName;
-            } else {
-                //Revisar si el jugador al que se intenta matar esta aun vivo.
-                if (!playerSlain.isAlive) console.log(`Hey ${playerSlain.nickName} Ya fue asesinado, la operacion no se puede completar`);
-                else {
-                    killResume();
-                    this.lastKiller = this.nickName;
-                }
-            }
+          killResume();
+          this.lastKiller = this.nickName;
         }
-    } 
+      }
+    }
+  }
 }
 //Jugadores del primer equipo, Siendo estos identificados con true.
 let player_1 = new Player("Nick 1", true, "Skin default", true);
