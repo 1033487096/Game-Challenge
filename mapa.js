11d0df97a948028c05e848f5080c4af4613409b0
Juan <97717969+1033487096@users.noreply.github.com>
2022-10-24 19:08:10 -0500

    Delete mapa.js

--- mapa.js
+++
@@ -1,9 +0,0 @@
-class mapa {
-
-    constructor(nombre, equipos, tamaño) {
-        this.nombre = nombre;
-        this.equipos = equipos;
-        this.tamaño = tamaño;
-    }
-}
-module.exports = mapa;
